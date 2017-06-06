8a4f5217b7041ec41dbd38f00b7ada1e57611301
James Campbell <james@jamescampbell.us>
2017-06-06 09:36:48 -0400

    added example looping through download folders for .jpg

--- examples/hello_folders/src/main.rs
+++ examples/hello_folders/src/main.rs
@@ -1,10 +1,11 @@
-use std::fs;
+extern crate glob;
+use self::glob::glob;
 
 fn main() {
-    let paths = fs::read_dir("./").unwrap();
-
-    for path in paths {
-        println!("Name: {}", path.unwrap().path().display())
+for entry in glob("/Users/jc/Downloads/**/*.jpg").expect("Failed to read glob pattern") {
+    match entry {
+        Ok(path) => println!("{:?}", path.display()),
+        Err(e) => println!("{:?}", e),
     }
 }
-
+}
