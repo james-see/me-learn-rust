9d9e44719cf6a44460a82ea94f728c9fc1e84462
James Campbell <james@jamescampbell.us>
2017-11-24 23:53:05 -0500

    updates to FtoC

--- examples/FtoC/src/main.rs
+++ examples/FtoC/src/main.rs
@@ -1,12 +1,11 @@
 extern crate rand;
-use std::io;
 use rand::Rng;
 
 fn main() {
     println!("Hello, converting F to C!");
     let mut fahrenheit : f64 = rand::thread_rng().gen_range(1.0, 100.0);
     fahrenheit = fahrenheit.round();
-    println!("Starting fahrenheit temp: {}", fahrenheit);
+    println!("Starting Fahrenheit temp to convert: {}", fahrenheit);
     let mut celcius : f64 = (fahrenheit - 32.0);
     celcius = (celcius / 1.8 as f64);
     celcius = celcius.round();
