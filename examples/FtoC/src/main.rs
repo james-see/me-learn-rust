e6edd5cbfeb051fd0dc65a03da946bc52ef02004
James Campbell <james@jamescampbell.us>
2017-11-25 23:19:46 -0500

    fixed f to c to accept input

--- examples/FtoC/src/main.rs
+++ examples/FtoC/src/main.rs
@@ -1,13 +1,26 @@
-extern crate rand;
-use rand::Rng;
+use std::io;
 
 fn main() {
     println!("Hello, converting F to C!");
-    let mut fahrenheit : f64 = rand::thread_rng().gen_range(1.0, 100.0);
-    fahrenheit = fahrenheit.round();
+    loop {
+    println!("Fahrenheit value in decimal format: ");
+    let mut guesser = String::new();
+    io::stdin().read_line(&mut guesser).expect("Failed to read line");
+
+    let guess: f64 = match guesser.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+    println!("this is the number: {}", guesser);
+    
+      //  let mut fahrenheit : f64 = rand::thread_rng().gen_range(1.0, 100.0);
+    let mut fahrenheit : f64 = guess;
+    //fahrenheit = fahrenheit;
     println!("Starting Fahrenheit temp to convert: {}", fahrenheit);
-    let mut celsius : f64 = (fahrenheit - 32.0);
+    let mut celsius : f64 = fahrenheit - 32.0;
     celsius = (celsius / 1.8 as f64);
     celsius = celsius.round();
     println!("Fahrenheit {} in Celsius: {}", fahrenheit,celsius);
+    break;
+    }
 }
